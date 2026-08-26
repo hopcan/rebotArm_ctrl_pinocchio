e9c9a14fa192b87b78dbe4eb2c17fa7243e64628
PAN <nwkvo4@163.com>
2026-08-26 17:41:48 +0800

    change gravity_adm para

--- example/gravity_conpensation_adm.py
+++ example/gravity_conpensation_adm.py
@@ -16,13 +16,10 @@ from rebotArm_handle import reBotArm_handle
 import pinocchio_handle
 import time
 torque = [0]*7
-adm_pos = [0]*7
-vel_cur = [0]*7
-adm_scale = [1, 3,3, 10,10,10, 0]
-fric_comp =[0]*7
+adm_scale = [1, 3, 1 , 10,10,10, 0]
 gripper_pos = 0
 gripper_scale = 2.5
-# 用平滑的曲线代替硬截止
+
 def smooth_deadzone(x, threshold=0.02):
     if abs(x) < threshold:
         return 0.0
@@ -49,7 +46,6 @@ if __name__ == "__main__":
             # print(f"gravity torques : {full_torques}\n")
             for motor_id in list(range(1,8)):
                 torque[motor_id-1] = handle.motor_state[motor_id].torq
-                vel_cur[motor_id-1] = handle.motor_state[motor_id].vel
                 tmp = torque[motor_id-1] - full_torques[motor_id-1]
                 if motor_id == 7 :
                     tau_ext_processed = smooth_deadzone(tmp, 0.05)
@@ -60,18 +56,8 @@ if __name__ == "__main__":
                     #     print(f"gripper error : {gripper_pos}")
                 else:
                     tau_ext_processed = smooth_deadzone(tmp, 0.1)
-                        # 摩擦前馈补偿
-                    vel = vel_cur[motor_id - 1]
-                    # 速度死区
-                    if abs(vel) < 0.01:  # 小于 0.005 rad/s 时，认为完全静止
-                        fric_comp[motor_id - 1] = 0.0
-                    else:
-                        # 库仑摩擦 + 粘滞摩擦
-                        fric_comp[motor_id - 1] = np.sign(vel).tolist() * 0.01 + 0.01 * vel
-                    # print(fric_comp)
                     
-                    full_torques[motor_id-1] = full_torques[motor_id-1] - tau_ext_processed / adm_scale[motor_id-1]# + fric_comp[motor_id-1]
-            print(vel_cur)
+                    full_torques[motor_id-1] = full_torques[motor_id-1] - tau_ext_processed / adm_scale[motor_id-1]
             handle.move_to_joint_positions(positions = motors_last_pos,torque = full_torques)
             # print(f"real torques : {torque}\n")
             # print(f"output torques : {full_torques}\n")
